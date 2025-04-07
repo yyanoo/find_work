ae50223b0c401276aa35ce3dd5c7c4e9d881c64a
yyanoo <78666961+yyanoo@users.noreply.github.com>
2025-04-07 08:20:53 +0000

    pull

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ createApp({
 
             mainTitle: "骰子魔物戰",
             subTitle: "點擊按鈕 進行投骰子 依照大小來決定傷害判例",
-        });
+        })
 
         // 玩家數據
         const player = reactive({
@@ -23,7 +23,7 @@ createApp({
             hp: 10,
             max_hp: 10,
             atk: 3
-        });
+        })
 
         // 怪物數據
         const monster = reactive({
@@ -31,17 +31,17 @@ createApp({
             hp:100,
             max_hp: 100,
             atk: 2
-        });
+        })
 
         //網頁狀態管理
         const textControlPanel = reactive({
             numRoll:0,
             textGameover:"Game Over"
-        });
+        })
 
         function rollDice() {
             return Math.floor(Math.random() * 6) + 1;
-        };
+        }
 
         // 檢查等級提升
         function checkLvlUp(){
@@ -52,7 +52,7 @@ createApp({
                 player.atk += 3;
                 player.hp = player.max_hp += 10;
             }
-        };
+        }
 
         // 玩家攻擊
         function playerAttack(){
@@ -61,17 +61,18 @@ createApp({
                 monster.hp -= player.atk*textControlPanel.numRoll;
                 monsterControlPanel();
             }
-        };
+        }
 
         //怪物狀態管理
         function monsterControlPanel(){
             player.hp -= monster.atk;
+            gameOver();
             // 怪物重生
             if(monster.hp <= 0){
                 player.exp += monster.get_exp;
                 monster.hp = monster.max_hp;
             }
-        };
+        }
 
         //玩家血量管理
         function hpControlPanel(){
@@ -81,10 +82,18 @@ createApp({
                     player.hp = player.max_hp;
                 }
             }
-        };
+        }
+
+        function gameControlPanel(){
+            if(player.hp < 0){
+                player.hp = 0;
+                document.querySelector('#gameOver').innerText ='GameOver';
+                this.isDisabled = true;
+            }
+        }
 
         // 監聽等級變化
         watchEffect(checkLvlUp);
-        return {siteData, player, monster, textControlPanel, playerAttack, hpControlPanel, monsterControlPanel};
+        return {siteData, player, monster, textControlPanel, playerAttack, hpControlPanel, monsterControlPanel, gameControlPanel, isDisabled: false};
     }
 }).mount("#app");
